b509c2322b4d82ffb98e0b927ebd16c4881fb369
Oscar Garcia <oscargf1998@gmail.com>
2020-04-15 12:08:51 +0200

    coverage 100%

--- switcher_test.py
+++ switcher_test.py
@@ -1,54 +1,45 @@
 import unittest
-from .switcher import Switch, Default, DuplicatedCaseException
+from switcher import Switch, Default, DuplicatedCaseException
+
+
+def switch_func(options: int):
+    @Switch(options)
+    def switch(s):
+        @s.case(1)
+        def one():
+            return 1
+    return switch()
+
+
+def switch_with_default(options: int):
+    @Switch(options)
+    def switch(s):
+        @s.case(Default)
+        def default():
+            return 0
+    return switch()
 
 
 class SwitchTest(unittest.TestCase):
-
     def test_01_correct_case(self):
-        @Switch(1)
-        def switch(s):
-            @s.case(1)
-            def one():
-                return 1
-
-        self.assertEqual(switch(), 1)
+        self.assertEqual(switch_func(1), 1)
 
     def test_02_default_value(self):
-        @Switch(2)
-        def switch(s):
-            @s.case(1)
-            def one():
-                return 1
-
-        self.assertEqual(switch(), None)
+        self.assertEqual(switch_func(2), None)
 
     def test_03_custom_default(self):
-        @Switch(2)
-        def switch(s):
-            @s.case(1)
-            def one():
-                return 1
-
-            @s.case(Default)
-            def default():
-                return 0
-
-        self.assertEqual(switch(), 0)
+        self.assertEqual(switch_with_default(1), 0)
 
     def test_04_duplicated_case_exception(self):
         @Switch(2)
         def switch(s):
             @s.case(1)
             def one():
-                return 1
+                """ Do nothing """
 
             @s.case(1)
             def one2():
-                return 1
+                """ Do nothing """
 
         with self.assertRaises(DuplicatedCaseException):
             switch()
-
-
-if __name__ == '__main__':
-    unittest.main()
